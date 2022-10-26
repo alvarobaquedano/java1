c7ecd6a9246b4cfd5220fce0f0dba22fb6de4ead
cfgs <cfgs@info1-pc29>
2022-10-26 13:24:32 +0200

    array

---
+++ PrimerosPasos/src/array/Arrays.java
@@ -0,0 +1,22 @@
+package array;
+
+import java.util.Scanner;
+
+
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Introduce el número total de notas:");
+		
+		Double numnotas = entrada.nextDouble();
+		do {
+			
+		}
+		while (numnotas > 0);
+	}
+
+}
