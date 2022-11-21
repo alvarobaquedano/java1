9687ac9106e33b1ce7843ed6400abcba0313d832
cfgs <cfgs@info1-pc29>
2022-11-21 14:23:55 +0100

    compararaarrays

---
+++ PrimerosPasos/src/array/OrdenarArrays.java
@@ -0,0 +1,26 @@
+package array;
+
+import java.util.Scanner;
+
+public class OrdenarArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		int numero2 = 0;
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Introduce el número total de elementos");
+		numero2 = entrada.nextInt();
+		
+		int [] numero = new int[numero2];
+		
+		for (i = 0; i < numero2; i++) {
+			System.out.println("Introduce número(" +(i+1)+ "): ");
+			
+			if 
+		}
+	}
+
+}
