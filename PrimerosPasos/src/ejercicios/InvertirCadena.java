516eb2af8de70bf3c3d4e14bbcc68b00fb001981
alvarobaqudano <alvaro.baquedano.dosa@gmail.com>
2022-12-13 10:02:25 +0100

    invertir cadena

--- PrimerosPasos/src/ejercicios/InvertirCadena.java
+++ PrimerosPasos/src/ejercicios/InvertirCadena.java
@@ -1,19 +1,24 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class InvertirCadena {
 
 	public static void main(String[] args) {
 
-		String cadena = "Hola";
-		
-		String invertida = "";
-
-		for (int indice = cadena.length() - 1; indice >= 0; indice--) {
+		try (Scanner entrada = new Scanner(System.in)) {
+			System.out.println("Introduce una palabra");
+			String cadena = entrada.nextLine();
 			
-			invertida += cadena.charAt(indice);
+			String invertida = "";
+
+			for (int indice = cadena.length() - 1; indice >= 0; indice--) {
+				
+				invertida += cadena.charAt(indice);
+			}
+			System.out.println("Cadena original: " + cadena);
+			System.out.println("Cadena invertida: " + invertida);
 		}
-		System.out.println("Cadena original: " + cadena);
-		System.out.println("Cadena invertida: " + invertida);
 
 
 	}
